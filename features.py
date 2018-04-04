e62c266604277fee9ba74231b9b63125da77d3b4
Varun Raval <vraval48@gmail.com>
2018-04-04 19:52:38 -0400

    created file for features

---
+++ features.py
@@ -0,0 +1,21 @@
+
+
+"""
+
+Number of pieces
+Number of kings
+Number of pawns
+
+Number of agent's pieces in attack
+Number of pieces agent is attacking
+
+"""
+
+def feature_num_pieces(state, cur_player):
+	"""
+	state: the state of the game
+	cur_player: True if this feature is for the player whose turn is there in given state
+
+	Returns: Number of pieces of the appropriate player
+	"""
+	pass
